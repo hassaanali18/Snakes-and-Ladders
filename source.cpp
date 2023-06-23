c66e94f9c07219990dabd758403010b17e69168b
Syed Muhammad Hassaan Ali <136245885+hassaanali18@users.noreply.github.com>
2023-06-23 22:41:58 +0500

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+#include<ctime>
+#include"Header.h"
+using namespace std;
+
+int main()
+{
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << " : :      " << char(254) << "                                     : :" << endl;
+	cout << " : :                                            : :" << endl;
+	cout << " : :                               " << char(254) << "            : :" << endl;
+	cout << " : :                                            : :" << endl;
+	cout << " : :             Rock  Paper  Scissors          : :" << endl;
+	cout << " : :             (Project Third Wife)           : :" << endl;
+	cout << " : :   " << "<" << "                                        : :" << endl;
+	cout << " : :                             " << "*" << "              : :" << endl;
+	cout << " : :                                            : :" << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << endl << endl << endl;
+	Sleep(500);
+
+	SetConsoleTextAttribute(hStdOut, 3);
+	system("cls");
+	cout << endl << endl << endl;
+	cout << "  = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << "  :      " << char(254) << "                                       :" << endl;
+	cout << " :                                              :  " << endl;
+	cout << "  :                               " << char(254) << "              :" << endl;
+	cout << " :                                              :  " << endl;
+	cout << "  :             Rock  Paper  Scissors            :" << endl;
+	cout << " :               (Project Third Wife)           :  " << endl;
+	cout << "  :   " << "*" << "                                          :" << endl;
+	cout << " :                               " << "<" << "              :  " << endl;
+	cout << "  :                                              :" << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << "  = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << endl << endl << endl;
+	Sleep(500);
+
+	SetConsoleTextAttribute(hStdOut, 4);
+	system("cls");
+	cout << endl << endl << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << "  = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << " :        " << "*" << "                                     :  " << endl;
+	cout << "   :                                              :" << endl;
+	cout << " :                                 " << char(254) << "            :  " << endl;
+	cout << "   :                                              :" << endl;
+	cout << " :               Rock  Paper  Scissors          :  " << endl;
+	cout << "   :             (Project Third Wife)             :" << endl;
+	cout << " :     " << char(254) << "                                        :  " << endl;
+	cout << "   :                             " << "<" << "                :" << endl;
+	cout << " :                                              :  " << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << "  = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << endl << endl << endl;
+	Sleep(500);
+
+	SetConsoleTextAttribute(hStdOut, 6);
+	system("cls");
+	cout << endl << endl << endl;
+	cout << "  = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << "  :      " << char(254) << "                                       :" << endl;
+	cout << " :                                              :  " << endl;
+	cout << "  :                               " << char(254) << "              :" << endl;
+	cout << " :                                              :  " << endl;
+	cout << "  :             Rock  Paper  Scissors            :" << endl;
+	cout << " :               (Project Third Wife)           :  " << endl;
+	cout << "   :   " << "*" << "                                          :" << endl;
+	cout << " :                     " << "<" << "              :  " << endl;
+	cout << "   :                                              :" << endl;
+	cout << "  = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << endl << endl << endl;
+	Sleep(500);
+
+	SetConsoleTextAttribute(hStdOut, 3);
+	system("cls");
+	cout << endl << endl << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << "   = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << " :        " << "*" << "                                     :  " << endl;
+	cout << "  :                                              :" << endl;
+	cout << " :                                 " << char(254) << "            :  " << endl;
+	cout << "  :                                              :" << endl;
+	cout << " :               Rock  Paper  Scissors          :  " << endl;
+	cout << "  :             (Project Third Wife)             :" << endl;
+	cout << " :     " << char(254) << "                                        :  " << endl;
+	cout << "  :                             " << "<" << "                :" << endl;
+	cout << " :                                              :  " << endl;
+	cout << " = = = = = = = = = = = = = = = = = = = = = = = = = " << endl;
+	cout << "  = = = = = = = = = = = = = = = = = = = = = = = = =" << endl;
+	cout << endl << endl << endl;
+	Sleep(500);
+
+	SetConsoleTextAttribute(hStdOut, 4);
+	system("cls");
+	cout << endl << endl << endl;
+	cout << " ==================================================" << endl;
+	cout << " ==================================================" << endl;
+	cout << " : :      " << char(254) << "                                     : :" << endl;
+	cout << " : :                                            : :" << endl;
+	cout << " : :                               " << char(254) << "            : :" << endl;
+	cout << " : :                                            : :" << endl;
+	cout << " : :             Rock  Paper  Scissors          : :" << endl;
+	cout << " : :             (Project Third Wife)           : :" << endl;
+	cout << " : :   " << "<" << "                                        : :" << endl;
+	cout << " : :                             " << "*" << "              : :" << endl;
+	cout << " : :                                            : :" << endl;
+	cout << " ==================================================" << endl;
+	cout << " ==================================================" << endl;
+	cout << endl << endl;
+	Sleep(500);
+
+	SetConsoleTextAttribute(hStdOut, 7);
+
+	menu();
+
+	return 0;
+}
